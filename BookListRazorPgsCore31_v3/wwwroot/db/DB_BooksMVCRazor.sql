a3f29dc5a4ad9208bb81608182efa6824b266af8
TGitVision <60406685+TGitVision@users.noreply.github.com>
2020-11-20 17:07:09 -0500

    Added db sql script in wwwroot\db folder

---
+++ BookListRazorPgsCore31_v3/wwwroot/db/DB_BooksMVCRazor.sql
@@ -0,0 +1,32 @@
+USE [BookListRazor]
+GO
+
+/****** Object:  Table [dbo].[ytRZMVC_Books]    Script Date: 11/20/2020 5:01:15 PM ******/
+SET ANSI_NULLS ON
+GO
+
+SET QUOTED_IDENTIFIER ON
+GO
+
+CREATE TABLE [dbo].[ytRZMVC_Books](
+	[Book_ID] [int] IDENTITY(1,1) NOT NULL,
+	[Name] [nvarchar](60) NOT NULL,
+	[Author] [nvarchar](60) NOT NULL,
+	[ISBN] [nvarchar](25) NOT NULL,
+ CONSTRAINT [PK_ytRZMVC_Books] PRIMARY KEY CLUSTERED 
+(
+	[Book_ID] ASC
+)WITH (PAD_INDEX = OFF, STATISTICS_NORECOMPUTE = OFF, IGNORE_DUP_KEY = OFF, ALLOW_ROW_LOCKS = ON, ALLOW_PAGE_LOCKS = ON) ON [PRIMARY]
+) ON [PRIMARY]
+GO
+
+ALTER TABLE [dbo].[ytRZMVC_Books] ADD  CONSTRAINT [DF_ytRZMVC_Books_Name]  DEFAULT ('') FOR [Name]
+GO
+
+ALTER TABLE [dbo].[ytRZMVC_Books] ADD  CONSTRAINT [DF_ytRZMVC_Books_Author]  DEFAULT ('') FOR [Author]
+GO
+
+ALTER TABLE [dbo].[ytRZMVC_Books] ADD  CONSTRAINT [DF_ytRZMVC_Books_ISBN]  DEFAULT ('') FOR [ISBN]
+GO
+
+
